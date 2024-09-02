dbd6f5832121102c033477816a458f5c78ba3725
RM3 <roger@studioavante.com>
2024-09-02 14:28:27 -0400

    added devs script for localhost https

--- client/vite.https.config.ts
+++ client/vite.https.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     react(),
     wasm(),
     topLevelAwait(),
+    mkcert(),
   ],
 });
